fd1a2d74322ece2b702b6b14660ddec8fd8fe933
Savadatti <Savadatti@Savadatti-PC>
2022-07-07 16:41:06 +0530

    hello

---
+++ Assign2/src/Generic/Framework_contains.java
@@ -0,0 +1,11 @@
+package Generic;
+
+public interface Framework_contains {
+	String gecko_key="webdriver.gecko.driver";
+	String gecko_value="./software/geckodriver.exe";
+	
+	String chrome_key="webdriver.chrome.driver";
+	String chrome_value="./software/chromedriver.exe";
+	
+	String base_url="https://www.facebook.com";
+}
